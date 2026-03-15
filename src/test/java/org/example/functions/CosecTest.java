481244e144950437a157883c86f69fdb940b3c4b
kirill <kirill.rasshivalov@gmail.com>
2026-03-15 12:55:16 +0300

    Добавил тесты на косеканс.

---
+++ src/test/java/org/example/functions/CosecTest.java
@@ -0,0 +1,74 @@
+package org.example.functions;
+
+import org.example.base_functions.Sin;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static junit.framework.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class CosecTest {
+
+    private static final double DELTA = 1e-8;
+
+    private static final double HIGH_PRECISION = 1e-10;
+
+    private static final double PI = Math.PI;
+
+    private Cosec cosecCalculator;
+
+    @BeforeEach
+    void setUp() {
+        this.cosecCalculator = new Cosec(new Sin());
+    }
+
+    @Test
+    @DisplayName("Проверка работаспособности на значениях синуса из таблицы.")
+    void testOnTableSin() {
+        assertEquals(1, cosecCalculator.calculate(PI / 2, HIGH_PRECISION), DELTA);
+        assertEquals(-1, cosecCalculator.calculate((PI * 3) / 2, HIGH_PRECISION), DELTA);
+    }
+
+    @Test
+    @DisplayName("Проверка достаточности предыдущих тестов с помощью периода.")
+    void testProvePrev() {
+        assertEquals(cosecCalculator.calculate(PI / 2, HIGH_PRECISION), cosecCalculator.calculate(PI / 2 + 2 * PI, HIGH_PRECISION), DELTA);
+        assertEquals(cosecCalculator.calculate(PI / 2, HIGH_PRECISION), cosecCalculator.calculate(PI / 2 - 2 * PI, HIGH_PRECISION), DELTA);
+        assertEquals(cosecCalculator.calculate((PI * 3) / 2, HIGH_PRECISION), cosecCalculator.calculate((PI * 3) / 2 + 2 * PI, HIGH_PRECISION), DELTA);
+        assertEquals(cosecCalculator.calculate((PI * 3) / 2, HIGH_PRECISION), cosecCalculator.calculate((PI * 3) / 2 - 2 * PI, HIGH_PRECISION), DELTA);
+    }
+
+    @Test
+    @DisplayName("Проверка точек выходящих за ОДЗ.")
+    void testIncorrectValues() {
+        Throwable error =  assertThrows(ArithmeticException.class, () -> {cosecCalculator.calculate(0, HIGH_PRECISION);});
+        assertThrows(ArithmeticException.class, () -> {
+            cosecCalculator.calculate(0, HIGH_PRECISION);
+        });
+        assertThrows(ArithmeticException.class, () -> {
+            cosecCalculator.calculate(PI, HIGH_PRECISION);
+        });
+        assertEquals("Значение синуса не должно быть равно нулю.", error.getMessage());
+    }
+
+    @Test
+    @DisplayName("Проверка нечетности функции.")
+    void testOddFunction() {
+        double x = PI / 3;
+        double cosec = cosecCalculator.calculate(x, HIGH_PRECISION);
+        double cosecMinusX = cosecCalculator.calculate(-x, HIGH_PRECISION);
+        assertEquals(cosecMinusX, -cosec, DELTA);
+    }
+
+    @Test
+    @DisplayName("Сравнение с оргинальным косекансом.")
+    void testOriginal() {
+        double x1 = PI / 4;
+        double x2 = 12;
+        double x3 = 2 * PI / 6;
+        assertEquals(cosecCalculator.etalon(x1), cosecCalculator.calculate(x1, HIGH_PRECISION), DELTA);
+        assertEquals(cosecCalculator.etalon(x2), cosecCalculator.calculate(x2, HIGH_PRECISION), DELTA);
+        assertEquals(cosecCalculator.etalon(x3), cosecCalculator.calculate(x3, HIGH_PRECISION), DELTA);
+    }
+}
